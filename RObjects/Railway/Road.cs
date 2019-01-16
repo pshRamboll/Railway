d6f92fe317c94472289f0f3875da0031e27a55fb
pshRamboll <45963958+pshRamboll@users.noreply.github.com>
2019-01-16 14:18:03 +0100

    Added Road object

---
+++ RObjects/Railway/Road.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rhino.Geometry;
+
+namespace RObjects
+{
+    public class Road 
+    {
+        public string Name { get; set; } = "";
+        public double Height { get; set; } = 0.0;
+        public Curve Curve { get; set; }
+    }
+}
